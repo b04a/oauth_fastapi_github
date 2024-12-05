360796633e31644c443f50e10b3f538c7a4852d5
+futurehendrix <bochka13@list.ru>
2024-12-05 15:54:21 +0300

    create /login page (oauth github)

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ import requests
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+GITHUB_CLIENT_ID = "your CLIENT_ID!"
+GITHUB_CLIENT_SECRET = "your CLIENT_SECRET!"
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/login")
+async def login():
+    return RedirectResponse(f"https://github.com/login/oauth/authorize?client_id={GITHUB_CLIENT_ID}")
 
